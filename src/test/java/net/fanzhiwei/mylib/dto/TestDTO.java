ae46740641787a614489bcc096698856b3b9d821
fanzhiwei2010 <fanzhiwei2010@gmail.com>
2014-05-29 07:52:18 +0000

    增加guava测试用例

---
+++ src/test/java/net/fanzhiwei/mylib/dto/TestDTO.java
@@ -0,0 +1,18 @@
+package net.fanzhiwei.mylib.dto;
+
+/**
+ * User: zhiweifan
+ * Date: 14-5-26
+ * Time: 上午11:37
+ */
+public class TestDTO {
+    private String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+}
