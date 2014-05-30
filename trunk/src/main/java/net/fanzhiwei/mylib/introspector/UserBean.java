396e685cbe483d22e21bc27914541817b4021a24
fanzhiwei2010@gmail.com <fanzhiwei2010@gmail.com@28f6427c-f533-900b-41b8-84ad8fcf2764>
2014-05-30 07:58:58 +0000

    文件位置调整

---
+++ trunk/src/main/java/net/fanzhiwei/mylib/introspector/UserBean.java
@@ -0,0 +1,29 @@
+package net.fanzhiwei.mylib.introspector;
+
+/**
+ * User: zhiweifan
+ * Date: 14-5-30
+ * Time: 上午10:56
+ */
+public class UserBean {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
+
